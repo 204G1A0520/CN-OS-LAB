30cb61ad94c0a1f1548cbac04d1fdb41bf57b0f9
204G1A0520 <94513237+204G1A0520@users.noreply.github.com>
2022-09-13 11:43:39 +0530

    sjf.c

--- exp1/sjf.c
+++ exp1/sjf.c
@@ -1,8 +1,8 @@
 /* Name Of the Candidate: BHARATH KUMAR K J
    Roll No: 204G1A0520
    Title of the Experiment: CPU Scheduling Using SJF Algorithm
-   Date of Creation: 30-08-2022
-   Date of Execution: 30-08-2022
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
 */
 // Source Code
 #include <stdio.h>
